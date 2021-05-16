cca9c3f429920ec00b00d7e6da5f8b4d954ae23e
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2021-05-16 13:33:31 +0100

    feat(test-tooling): faio features and improvements
    Adds several improvements:
    1) destroys faio volume when tests end
    2) allows to recover a test ledger (setContainer)
    3) allows to add organizations
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-test-tooling/src/main/typescript/public-api.ts
+++ packages/cactus-test-tooling/src/main/typescript/public-api.ts
@@ -48,6 +48,8 @@ export {
   IFabricTestLedgerV1ConstructorOptions,
   FABRIC_TEST_LEDGER_DEFAULT_OPTIONS,
   FABRIC_TEST_LEDGER_OPTIONS_JOI_SCHEMA,
+  STATE_DATABASE,
+  organizationDefinitionFabricV2,
 } from "./fabric/fabric-test-ledger-v1";
 
 export {
